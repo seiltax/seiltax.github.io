c46ee11e641ef76657efe1851211448e872e31e3
신나진 <113892130+nasneyland@users.noreply.github.com>
2024-04-22 13:06:11 +0900

    fix project

--- seil/flutter_service_worker.js
+++ seil/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "e17746e94b24f3eb2f75e50219ea621f",
-"index.html": "b0de797e15b26fcf32fc79960bf91300",
-"/": "b0de797e15b26fcf32fc79960bf91300",
-"main.dart.js": "4d993939bcbe25cf74f8490431cbab4f",
+"index.html": "d98d9161df0288c119bb62671e43ccd7",
+"/": "d98d9161df0288c119bb62671e43ccd7",
+"main.dart.js": "6516382b788e8a7ecf50b0f0aef0c84b",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "0c62d66aa73c32bad3cbd109725e5405",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
